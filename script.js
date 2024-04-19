9a0086c53beaf1794b330eb9d701c114fea0b76e
Federico-Pedro <125419127+Federico-Pedro@users.noreply.github.com>
2024-04-19 20:01:28 -0300

    Adds functionality to the number and operator buttons

--- script.js
+++ script.js
@@ -17,3 +17,21 @@ function divide(a, b){
     return a / b;
     }
 };
+
+
+
+const display = document.querySelector(".display");
+const keys = document.querySelectorAll(".btn");
+const c = document.querySelector(".c");
+const x = document.querySelector(".x");
+
+
+keys.forEach(function(key) {
+	key.addEventListener('click', function() {
+		display.innerHTML += key.innerText;
+	})
+});
+
+c.addEventListener('click', () => display.innerHTML = '');
+
+x.addEventListener('click', () => display.innerHTML = display.innerHTML.slice(0, -1));
