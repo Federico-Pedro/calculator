6b3e6a7ee8a9bd335963a593ddb355daa110f27e
Federico-Pedro <125419127+Federico-Pedro@users.noreply.github.com>
2024-04-22 11:02:48 -0300

    adds all functionality and style, almost ready

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+let num1;
+let num2;
+let operator;
+
 function add(a, b){
     return a + b;
 };
@@ -24,14 +28,39 @@ const display = document.querySelector(".display");
 const keys = document.querySelectorAll(".btn");
 const c = document.querySelector(".c");
 const x = document.querySelector(".x");
-
+const equal = document.querySelector(".equal");
+let input = "";
+let result = "";
 
 keys.forEach(function(key) {
+    
 	key.addEventListener('click', function() {
-		display.innerHTML += key.innerText;
+		input += key.innerText;
+        display.innerHTML = input;
 	})
 });
 
-c.addEventListener('click', () => display.innerHTML = '');
+c.addEventListener('click', () => clear());
 
-x.addEventListener('click', () => display.innerHTML = display.innerHTML.slice(0, -1));
+
+x.addEventListener('click', () => {
+    input = input.slice(0, -1);
+    display.innerHTML = input;
+});
+   
+function cut(input){
+    input = input.split('+');
+    num1 = input[0];
+    num2 = input[1];
+}
+
+equal.addEventListener('click',() => {
+    result = eval(input);
+    display.innerHTML = result;
+    input = result; 
+})
+
+function clear(){
+    display.innerHTML = '';
+    input = '';
+};
